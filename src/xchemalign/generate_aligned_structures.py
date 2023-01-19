cee599d8744d339086055d333042cb36c1c5292d
jfi22496 <conor.wild@sky.com>
2023-01-19 15:15:01 +0000

    debug

--- src/xchemalign/generate_aligned_structures.py
+++ src/xchemalign/generate_aligned_structures.py
@@ -66,6 +66,7 @@ def generate_aligned_structures(
 
     #
     for site in sites:
+        logger.debug(f"Site id is: {site.id}")
         site_dir = output_dir / f"{site.id}"
         if not site_dir.exists():
             os.mkdir(site_dir)
