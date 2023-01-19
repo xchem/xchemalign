baea6817198750d2c78420930524dbadf5e6a33e
jfi22496 <conor.wild@sky.com>
2023-01-19 14:35:07 +0000

    debug

--- src/xchemalign/get_alignable_sites.py
+++ src/xchemalign/get_alignable_sites.py
@@ -1,11 +1,51 @@
 from xchemalign.data import AlignableSite
 
 
-def get_alignable_sites(connected_components):
+def match_site(containing_site, contained_site):
+    if all(
+        [
+            ligand_id
+            for ligand_id in contained_site
+            if ligand_id in containing_site
+        ]
+    ):
+        return True
+    else:
+        return False
 
-    sites = []
+
+def get_alignable_sites(
+    connected_components, alignable_sites: list[AlignableSite] | None
+):
+
+    alignable_site_num: int = 0
+    if alignable_sites:
+        alignable_site_num = len(alignable_sites)
+    else:
+        alignable_sites = []
+
     for site_ligand_ids in connected_components:
-        sites.append(AlignableSite(id=0, name="", ligand_ids=site_ligand_ids))
 
-    return sites
-    ...
+        site_match: bool = False
+        for alignable_site in alignable_sites:
+            site_match = match_site(
+                containing_site=site_ligand_ids,
+                contained_site=alignable_site.ligand_ids,
+            )
+
+            # Update site if matched
+            if site_match:
+                for _ligand_id in site_ligand_ids:
+                    if _ligand_id not in alignable_site.ligand_ids:
+                        alignable_site.ligand_ids.append(_ligand_id)
+
+        # If not site has matched create a new alignable site
+        if not site_match:
+            alignable_sites.append(
+                AlignableSite(
+                    id=alignable_site_num, name="", ligand_ids=site_ligand_ids
+                )
+            )
+            alignable_site_num += 1
+
+    return alignable_sites
