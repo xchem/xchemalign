97be61b170daf0bcf28a5420e0e278c2f60c4d6b
ConorFWild <ConorFWild@users.noreply.github.com>
2023-01-31 14:38:45 +0000

    deploy: a60e9ce5c803c61a90de67fbfdef716d443e1a94

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev226+g4132208',
+    VERSION: '0.1.dev227+ga60e9ce',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
