e79712bd5e83d49f4b8ba00dc853994a5ac635a1
jfi22496 <conor.wild@sky.com>
2023-07-24 18:21:05 +0100

    Renamed to ligand_neighbourhood_alignment

--- src/ligand_neighbourhood_alignment/dt.py
+++ src/ligand_neighbourhood_alignment/dt.py
@@ -36,10 +36,10 @@ class LigandNeighbourhoodOutput:
 
     def to_dict(self):
         dic = {
-            'aligned_structures': {canonical_site_id:str(path) for canonical_site_id, path in self.aligned_structures},
-            'aligned_artefacts': {canonical_site_id:str(path) for canonical_site_id, path in self.aligned_artefacts},
-            'aligned_xmaps': {canonical_site_id:str(path) for canonical_site_id, path in self.aligned_xmaps},
-            'aligned_event_maps': {canonical_site_id:str(path) for canonical_site_id, path in self.aligned_event_maps},
+            'aligned_structures': {canonical_site_id:str(path) for canonical_site_id, path in self.aligned_structures.items()},
+            'aligned_artefacts': {canonical_site_id:str(path) for canonical_site_id, path in self.aligned_artefacts.items()},
+            'aligned_xmaps': {canonical_site_id:str(path) for canonical_site_id, path in self.aligned_xmaps.items()},
+            'aligned_event_maps': {canonical_site_id:str(path) for canonical_site_id, path in self.aligned_event_maps.items()},
         }
         return dic
 
