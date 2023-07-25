fbb20082796e85c5e5553db51502c6e699514e54
jfi22496 <conor.wild@sky.com>
2023-07-25 11:09:00 +0100

    Renamed to ligand_neighbourhood_alignment

--- src/ligand_neighbourhood_alignment/cli.py
+++ src/ligand_neighbourhood_alignment/cli.py
@@ -1307,8 +1307,8 @@ def _load_reference_stucture_transforms(reference_structure_transforms_yaml):
         with open(reference_structure_transforms_yaml, 'r') as f:
             dic = yaml.safe_load(f)
 
-        for reference_structure_id, reference_structure_transform_info in dic.items():
-            reference_structure_transforms[reference_structure_id] = dt.Transform.from_dict(reference_structure_transform_info)
+        for reference_structure_transform_id, reference_structure_transform_info in dic.items():
+            reference_structure_transforms["~".split(reference_structure_transform_id)] = dt.Transform.from_dict(reference_structure_transform_info)
 
     return reference_structure_transforms
 
