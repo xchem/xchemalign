71111901a23ac61006223c55b990d4f289c9e151
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-04-17 16:56:47 +0100

    New approach to treat version differently in fs model

---
+++ src/ligand_neighbourhood_alignment/alignment_heirarchy.py
@@ -0,0 +1,10 @@
+from ligand_neighbourhood_alignment import dt
+
+def _derive_alignment_heirarchy(assemblies: dict[str, dt.Assembly]):
+    # The Alignment hierarchy is the graph of alignments one must perform in order to get from
+    # a ligand canonical site to the Reference Assembly Frame
+
+    #
+
+
+    ...
